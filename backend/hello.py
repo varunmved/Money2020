cf2448d52d5ecb260eadb2e422f9e5ead210cc76
Varun Ved <varunmved@gmail.com>
2015-10-24 15:47:27 -0700

    random heartrate and time being setn

--- backend/hello.py
+++ backend/hello.py
@@ -1,14 +1,30 @@
 import requests
 import json
+import calendar
+import time
+import random 
 from firebase import firebase
+
 
 firebase = firebase.FirebaseApplication('https://baemoney2020.firebaseio.com', None)
 
+def putAppleWatch(firebase):
+    a = calendar.timegm(time.gmtime())
+    b = random.randint(80,100)
+    data = {str(a):str(b)}
+    data2 = {'a':'b'}
+    print(data)
+    print(data2)
+    #result = firebase.post('/watch',a,{'print':'pretty'},{'X_FANCY_HEADER': 'VERY FANCY'})
+    result = firebase.post('/watch',data)
+    #result = firebase.post('/watch',a)
+    
 
 def getFromAppleWatch(firebase):
-    result = firebase.get('/users',None)
-    a= json.dumps(result)
-    print(a[0])
+    result = firebase.get('/watch',None)
+    a=(json.dumps(result))
+    print(a)
+    
 
 def processAppleWatch():
     print('ayy')
@@ -16,5 +32,7 @@ def processAppleWatch():
 def putToFirebase():
     print('lmao')
 
-
-getFromAppleWatch(firebase)
+for i in range (0,60):
+    time.sleep(1)
+    putAppleWatch(firebase)
+#getFromAppleWatch(firebase)
